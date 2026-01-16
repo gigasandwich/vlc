23cf5ff5e7bc8873cc4c668f93aade11396feb69
Yvan Noah <yvannandy@gmail.com>
2026-01-16 17:36:12 +0300

    Add backend via Spring Initializr
    - Springboot: 4.0.1
    - Java 21
    - Dependencies
    	- Spring web
    	- Spring data JPA
    	- PostgreSQL Driver
    	- Validation
    	- Spring Boot DevTools
    
    Had to put application.properties in .gitignore, so I added: application.properties.example

---
+++ back/src/main/java/mg/serve/vlc/VlcApplication.java
@@ -0,0 +1,13 @@
+package mg.serve.vlc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VlcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VlcApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/mg/serve/vlc/VlcApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.serve.vlc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VlcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
