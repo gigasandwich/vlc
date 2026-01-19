2bfa4e0fdd8f80dc23a54b776832d6f35de1bc7d
Yvan Noah <yvannandy@gmail.com>
2026-01-20 00:02:01 +0300

    Add user historic when creating account

--- back/src/main/java/mg/serve/vlc/util/RepositoryProvider.java
+++ back/src/main/java/mg/serve/vlc/util/RepositoryProvider.java
@@ -1,22 +1,20 @@
 package mg.serve.vlc.util;
 
-import mg.serve.vlc.repository.ExampleRepository;
-import mg.serve.vlc.repository.UserRepository;
+import mg.serve.vlc.repository.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import lombok.Data;
-import lombok.Getter;
 
 @Component
 public class RepositoryProvider {
     public static ExampleRepository exampleRepository;
     public static UserRepository userRepository;
+    public static UserHistoricRepository userHistoricRepository;
 
     @Autowired
-    public RepositoryProvider(ExampleRepository exampleRepository, UserRepository userRepository) {
+    public RepositoryProvider(ExampleRepository exampleRepository, UserRepository userRepository, UserHistoricRepository userHistoricRepository) {
         RepositoryProvider.exampleRepository = exampleRepository;
         RepositoryProvider.userRepository = userRepository;
+        RepositoryProvider.userHistoricRepository = userHistoricRepository;
     }
 }
