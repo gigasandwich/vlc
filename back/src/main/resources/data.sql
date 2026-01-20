ab854c87bda9cdcd0b579197e2575a99e18f9ca2
Xenon-AH3009 <xenon.ah.300904@gmail.com>
2026-01-20 10:43:17 +0300

    required data

--- back/src/main/resources/data.sql
+++ back/src/main/resources/data.sql
@@ -1,13 +1,40 @@
-INSERT INTO example (column1) VALUES
-('Sample data 1'),
-('Sample data 2'),
-(null);
-
-INSERT INTO config (key, value_, type, date_) VALUES
-('token_expiration', '180', 'integer', NOW());
-INSERT INTO config (key, value_, type, date_) VALUES
-('token_expiration', '60', 'integer', NOW());
-
-INSERT INTO role(label) VALUES
-('USER'),
-('ADMIN');
+INSERT INTO example (column1) VALUES							
+('Sample data 1'),							
+('Sample data 2'),							
+(null);							
+							
+INSERT INTO config (key, value_, type, date_) VALUES							
+('TOKEN_EXPIRATION', '180', 'integer', NOW()),							
+('LOGIN_ATTEMPT_LIMIT', '3', 'integer', NOW());							
+							
+INSERT INTO action(label) VALUES							
+('WRONG_LOGIN'),							
+('LOGIN_ATTEMPT_RESET');						
+							
+INSERT INTO role(label) VALUES							
+('USER'),							
+('ADMIN');							
+							
+INSERT INTO user_state (label) VALUES							
+('ACTIVE'),							
+('INACTIVE'),							
+('BLOCKED');							
+							
+-- ===============================							
+-- Users							
+-- ===============================							
+							
+INSERT INTO user_(email, password, username, user_state_id) VALUES							
+('admin@gmail.com', 'admin', 'admin', 1),							
+('user1@gmail.com', 'pass1', 'user1', 1),							
+('user2@gmail.com', 'pass2', 'user2', 1);							
+							
+INSERT INTO user_historic (email, password, username, date_, user_state_id, user_id) VALUES							
+('admin@gmail.com', 'admin', 'admin', NOW(), 1, 1),							
+('user1@gmail.com', 'pass1', 'user1', NOW(), 1, 2),							
+('user2@gmail.com', 'pass2', 'user2', NOW(), 1, 3);							
+							
+INSERT INTO user_role (role_id, user_id) VALUES							
+(2, 1),							
+(1, 2),							
+(1, 3);					
---
+++ back/src/main/resources/data_recapGlob.sql
@@ -0,0 +1,21 @@
+-- =========================
+-- DONNÉES DE TEST
+-- =========================
+
+-- États des points
+INSERT INTO point_state (label, order_, progress) VALUES
+('Nouveau', 1, 0.25),
+('En cours', 2, 0.5),
+('Terminé', 3, 1);
+
+-- Types de points
+INSERT INTO point_type (label) VALUES
+('Industriel'),
+('Résidentiel');
+-- Points
+INSERT INTO point
+(date_, surface, budget, coordinates, user_id, point_state_id, point_type_id)
+VALUES
+(NOW(), 1000, 50000, ST_SetSRID(ST_MakePoint(2.35, 48.85), 4326), 1, 1, 1),
+(NOW(), 2000, 120000, ST_SetSRID(ST_MakePoint(2.36, 48.86), 4326), 1, 2, 1),
+(NOW(), 1500, 80000, ST_SetSRID(ST_MakePoint(2.37, 48.87), 4326), 2, 3, 2);
