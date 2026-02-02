d44049bacd1eae0c29bb7fa442e01b5dee298134
Xenon-AH3009 <xenon.ah.300904@gmail.com>
2026-02-02 22:37:05 +0300

    Update recapGlob.tsx
    integration

--- front/src/pages/dashboard/recapGlob.tsx
+++ front/src/pages/dashboard/recapGlob.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { backendURL } from '../../constant'
 
 interface recapGlobProps {
@@ -12,26 +12,23 @@ export default function RecapGlob({ onResponse }: recapGlobProps) {
   const [totalBudget,setTotalBudget] = useState(0)
   const [isLoading, setIsLoading] = useState(false)
 
-  async function submit(e: React.FormEvent) {
-    e.preventDefault()
-    setIsLoading(true)
-    setNbPoints(0)
-    setTotalSurface(0)
-    setAvgProgress(0)
-    setTotalBudget(0)
-    /*try {
-        
-      const params = new URLSearchParams()
-      params.append('email', email)
-      params.append('password', password)
-
-      const r = await fetch(`${backendURL}/auth/sign-in`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: params.toString(),
+  useEffect(() => {
+    const fetchSummary = async () => {
+      setIsLoading(true)
+    try {
+      const r = await fetch(`${backendURL}/points/summary`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
       })
+
       const j = await r.json()
+
       if (j.status === 'success') {
+        setNbPoints(j.data.nbPoints)
+        setTotalSurface(j.data.totalSurface)
+        setAvgProgress(j.data.avgProgress)
+        setTotalBudget(j.data.totalBudget)
+
         onResponse(j, 'success')
       } else {
         onResponse(j, 'error')
@@ -40,26 +37,33 @@ export default function RecapGlob({ onResponse }: recapGlobProps) {
       onResponse({ error: String(err) }, 'error')
     } finally {
       setIsLoading(false)
-    }*/
+    }
   }
+    fetchSummary()
+  },[])
 
   return (
     <div className="recapGlob-view">
       <h2 style={{ marginBottom: '16px', textAlign: 'center' }}>Récapitulation actuel</h2>
       <table>
-        <tr>
+        <thead>
+          <tr>
             <th>Nombre de points</th>
             <th>Total de surface</th>
             <th>Moyenne des avancements</th>
             <th>Total de budget</th>
-        </tr>
-        <tr>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
             <td>{nbPoints}</td>
             <td>{totalSurface}</td>
             <td>{avgProgress}</td>
             <td>{totalBudget}</td>
-        </tr>
+          </tr>
+        </tbody>
       </table>
+
     </div>
   )
 }
